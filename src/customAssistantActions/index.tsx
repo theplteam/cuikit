541d64a128e7944ee7a843aa9a8cc5d888cebacf
samiy-lu4schiy@yandex.ru <samiy-lu4schiy@yandex.ru>
2025-01-21 17:17:25 +0300

    new tests

---
+++ src/customAssistantActions/index.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import MdIconButton from '../../packages/chat-ui/src/ui/MdIconButton.tsx';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import BlindIcon from '@mui/icons-material/Blind';
+
+export const useCustomAssistantActions = () => {
+  return React.useMemo(() => [
+    <MdIconButton
+      onClick={() => console.log('click AcUnitIcon')}
+      key={'AcUnitIcon'}
+    >
+      <AcUnitIcon />
+    </MdIconButton>,
+    <MdIconButton
+      onClick={() => console.log('click BlindIcon')}
+      key={'BlindIcon'}
+    >
+      <BlindIcon />
+    </MdIconButton>
+  ], []);
+}
