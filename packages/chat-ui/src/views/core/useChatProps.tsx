d71598dd30090c7f9ec5f0dfc525b7d4e9edce34
samiy-lu4schiy@yandex.ru <samiy-lu4schiy@yandex.ru>
2025-02-06 14:26:59 +0300

    Refactor Dialogue and enhance ChatProps documentation
    Updated `Dialogue` class to simplify its generic types and improve maintainability. Expanded `ChatPropsTypes` and `ChatUsersProps` with detailed documentation for clearer usage and better developer experience.

--- packages/chat-ui/src/views/core/useChatProps.tsx
+++ packages/chat-ui/src/views/core/useChatProps.tsx
@@ -10,28 +10,74 @@ import { SlotPropsType } from './SlotPropsType';
 import { ApiRefType } from './useInitializeApiRef';
 
 type RequiredProps<D extends DialogueAbstract> = {
+  /**
+   * Dialogues list
+   * @required
+   */
   readonly dialogues: readonly D[];
+  /**
+   * Current dialogue
+   * @required
+   */
   dialogue: D | undefined;
+  /**
+   * Set current dialogue
+   * @required
+   * @param dialogue
+   */
   setDialogue: (dialogue: D) => void;
 };
 
 // используется внутри библиотеки
 export type ChatPropsTypes<D extends DialogueAbstract> = {
   loading: boolean;
-  modelProps?: ChatModelProps<D>;
+  /**
+   * A model that comprehensively controls dialogues: initiating a new conversation, deleting one, and refining an existing dialogue.
+   * You may either provide your own model or retain the standard one.
+   */
   model?: ChatModel<D>;
+  /**
+   * Props for ChatModel. Provide dialogue management functions if you have not designated your own model
+   */
+  modelProps?: ChatModelProps<D>;
+  /**
+   * Action buttons for the assistant's message.
+   */
   assistantActions?: React.JSXElementConstructor<{ message: Message, dialogue: D }>[];
   userId?: UserIdType;
+  /**
+   * Runtime processing of the assistant's message.
+   * @param text
+   */
   proccessAssistantText?: (text: string) => string;
 } & RequiredProps<D>;
 
 // что передает пользователь
 export type ChatUsersProps<D extends DialogueAbstract> = {
+  /**
+   * ChatUI defaults to using the window for automatic conversation scrolling.
+   * if you have embedded the chat within your own component, supply the container's ref to allow for proper scroll management.
+   */
   scrollerRef?: React.RefObject<HTMLDivElement | null>;
+  /**
+   * The components used for each slot inside. Can be instantiated with `useChatSlots`
+   */
   slots?: Partial<Omit<SlotsType<D>, 'core'>>;
-  coreSlots?: Partial<CoreSlots>;
+  /**
+   * The props used for each slot inside. Can be instantiated with `useChatSlots`
+   */
   slotProps?: Partial<SlotPropsType<D>>;
+  /**
+   * The components used for each core slot include Button, IconButton, etc. Can be instantiated with `useChatSlots` or `useChatCoreSlots`
+   */
+  coreSlots?: Partial<CoreSlots>;
+  /**
+   * The language in which the chat interface will be displayed.
+   */
   lang?: 'en' | 'ru' | LangKeys;
+  /**
+   * The ref object that allows ChatUI manipulation. Can be instantiated with `useChatContext`
+   */
   apiRef?: React.MutableRefObject<ApiRefType>;
 } & RequiredProps<D> & Partial<Omit<ChatPropsTypes<D>, 'slots' | 'coreSlots' | 'slotProps' | keyof RequiredProps<D>>>;
 
