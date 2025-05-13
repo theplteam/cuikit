6d6aee54d339f9f75ed6c84a6170586a79a680e8
Dmitriy <povorot14054@gmail.com>
2025-05-13 14:12:57 +0300

    fix console error

--- packages/chat-ui/src/views/chatUi/ChatPage.tsx
+++ packages/chat-ui/src/views/chatUi/ChatPage.tsx
@@ -3,12 +3,19 @@ import * as React from 'react';
 import ChatTheme from '../core/ChatTheme';
 import ChatUi, { ChatUiProps } from './ChatUi';
 import { Thread, Message } from '../../models';
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
 
 const ChatPage = <DM extends Message, DD extends Thread<DM>>(usersProps: ChatUiProps<DM, DD>) => {
+  const emotionCache = createCache({ key: 'css', prepend: true });
+  emotionCache.compat = true;
+
   return (
-    <ChatTheme>
-      <ChatUi {...usersProps} />
-    </ChatTheme>
+    <CacheProvider value={emotionCache}>
+      <ChatTheme>
+        <ChatUi {...usersProps} />
+      </ChatTheme>
+    </CacheProvider>
   );
 }
 
