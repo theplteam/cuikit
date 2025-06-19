3fc16f7d37d33139098fbc30b28839ef391b7ee8
arman.k <arman.k@playliner.com>
2025-06-19 13:26:19 +0300

    update MessageMarkdownUl rtl logic

--- packages/chat-ui/src/views/message/markdown/MessageMarkdownUl.tsx
+++ packages/chat-ui/src/views/message/markdown/MessageMarkdownUl.tsx
@@ -1,16 +1,22 @@
 import { styled } from '@mui/material/styles';
+import clsx from 'clsx';
+import React from 'react';
 
-const MessageMarkdownUl = styled('ul')(({ theme }) => ({
+const StyledUl = styled('ul')(({ theme }) => ({
   listStyle: 'none',
-  padding: 0,
+  padding: '0px 1.625em',
   '& li': {
     marginBottom: 8,
     '&:last-child': {
       marginBottom: 0,
     },
+    position: 'relative',
     ...theme.typography.body1,
     '&::before': {
       content: '""',
+      position: 'absolute',
+      top: theme.spacing(1),
+      left: theme.spacing(-3),
       width: 6,
       height: 6,
       margin: '2px 8px',
@@ -20,12 +26,16 @@ const MessageMarkdownUl = styled('ul')(({ theme }) => ({
     },
     'ul': {
       marginTop: theme.spacing(1.5),
-    }
+    },
+  },
+  '&.rtl-marker li::before': {
+    left: 'auto',
+    right: theme.spacing(-3),
   },
 }));
 
 const ChatMessageOl = styled('ol')(({ theme }) => ({
-  padding: '0px 1.625em ',
+  padding: '0px 1.625em',
   '& li': {
     marginBottom: 8,
     '&:last-child': {
@@ -41,5 +51,29 @@ const ChatMessageOl = styled('ol')(({ theme }) => ({
     },
   },
 }));
+
+const MessageMarkdownUl: React.FC<React.HTMLAttributes<HTMLUListElement>> = (props) => {
+  const checkRtlText = () => {
+    const rtlChars = /[\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC]/;
+    const item = React.Children.toArray(props.children)[0];
+
+    if (item) {
+      const li = item as React.ReactElement;
+      const isRtl = rtlChars.test(li.props.children);
+      return isRtl;
+    }
+
+    return false;
+  };
+
+  const isRtl = checkRtlText();
+
+  return (
+    <StyledUl
+      {...props}
+      className={clsx(props.className, { ['rtl-marker']: isRtl })}
+    />
+  );
+};
 
 export { MessageMarkdownUl, ChatMessageOl };
