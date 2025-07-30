989bf49548756795b071ca912372813592300353
arman.k <arman.k@playliner.com>
2025-07-30 13:27:55 +0300

    now all fine

--- packages/chat-ui/src/views/message/markdown/MessageMarkdownUl.tsx
+++ packages/chat-ui/src/views/message/markdown/MessageMarkdownUl.tsx
@@ -4,20 +4,20 @@ const MessageMarkdownUl = styled('ul')(({ theme }) => ({
   listStyle: 'none',
   paddingLeft: '1.625em',
   '& li': {
+    paddingLeft: 6,
     marginBottom: 8,
     '&:last-child': {
       marginBottom: 0,
     },
-    position: 'relative',
     ...theme.typography.body1,
+    position: 'relative',
     '&::before': {
       content: '""',
       position: 'absolute',
-      top: theme.spacing(1),
-      left: theme.spacing(-3),
+      top: theme.spacing(1.3),
+      left: theme.spacing(-2),
       width: 6,
       height: 6,
-      margin: '2px 8px',
       display: 'inline-block',
       borderRadius: '50%',
       background: theme.palette.primary.main,
@@ -29,9 +29,13 @@ const MessageMarkdownUl = styled('ul')(({ theme }) => ({
   '&:dir(rtl)': {
     paddingLeft: 0,
     paddingRight: '1.625em',
-    '& li::before': {
-      left: 'auto',
-      right: theme.spacing(-3),
+    '& li': {
+      paddingRight: 6,
+      paddingLeft: 0,
+      '&::before': {
+        left: 'auto',
+        right: theme.spacing(-2),
+      }
     }
   },
 }));
