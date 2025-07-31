8dda3b423f1d05b9a85b8ce273475ad96aa92738
arman.k <arman.k@playliner.com>
2025-07-31 12:33:15 +0300

    disableRtl

--- packages/chat-ui/src/views/message/markdown/MessageMarkdownWrapper.tsx
+++ packages/chat-ui/src/views/message/markdown/MessageMarkdownWrapper.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Box, { BoxProps } from "@mui/material/Box";
+import { useChatContext } from "../../../views/core/ChatGlobalContext";
 
-const MessageMarkdownWrapper: React.FC<BoxProps> = (p) => <Box {...p} dir='auto' />
+const MessageMarkdownWrapper: React.FC<BoxProps> = (p) => {
+  const { disableRtl } = useChatContext();
+  return <Box {...p} dir={disableRtl ? 'ltr' : 'auto'} />
+}
 
 export default MessageMarkdownWrapper;
